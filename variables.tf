3c6dc964061e8c53f5eed9e1c7c6ffff3b86fe8e
temiloluwao <temiloluwao@phillipsoutsourcing.net>
2023-10-17 14:52:31 +0100

    kopied

--- variables.tf
+++ variables.tf
@@ -4,10 +4,3 @@ variable "user_name" {
   default = "terraform_temi"
 }
 
-variable "access_key" {
-  default = "AKIAYFSLFDZ3DA2JL7TO"
-}
-
-variable "secret_key" {
-  default = "+q98stIK0ugW5AE3+RYY5aJiqmQhdCPqaoBvSMB5"
-}
